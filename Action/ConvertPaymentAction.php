da324a150a03865ecc138d93107396e98391a6e6
victor <v.adomian@tradenart.com>
2023-09-11 11:01:23 +0200

    Compatibilité commande invité MDF

--- Action/ConvertPaymentAction.php
+++ Action/ConvertPaymentAction.php
@@ -26,51 +26,18 @@ class ConvertPaymentAction extends GatewayAwareAction implements ActionInterface
         RequestNotSupportedException::assertSupports($this, $request);
 
         /** @var PaymentInterface $payment */
-        $payment = $request->getSource();        
+        $payment = $request->getSource();
         $details = ArrayObject::ensureArrayObject($payment->getDetails());
-        
+
         $this->gateway->execute($currency = new GetCurrency($payment->getCurrencyCode()));
-        
+
         $details['currency'] = $currency->alpha3;
         $details['amount'] = $payment->getTotalAmount();
-        
-        $details['metadata'] = array(
-            'commande_id' =>  $payment->getCommande()->getId(),
-        );
-        
-        $adrFact = $payment->getCommande()->getAdresseFacturation();
-        
-        $details['billing'] = array(
-            'first_name' => $adrFact->getPrenom(),
-            'last_name' => $adrFact->getNom(),
-            'email' => $payment->getCommande()->getUtilisateur()->getEmail(),
-            'address1' => $adrFact->getAdresse1(),
-            'address2' => $adrFact->getAdresse2(),
-            'postcode' => $adrFact->getCp(),
-            'city' => $adrFact->getVille(),
-            'country' => $adrFact->getPays(),
-        );
-        
-        $adrLivr = $payment->getCommande()->getAdresseLivraison();
-        
-        $delivery_type = 'OTHER';
-        
-        if($adrFact->getId() == $adrLivr->getId()){
-            $delivery_type = 'BILLING';
-        }
-        
-        $details['shipping'] = array(
-            'first_name' => $adrLivr->getPrenom(),
-            'last_name' => $adrLivr->getNom(),
-            'email' => $payment->getCommande()->getUtilisateur()->getEmail(),
-            'address1' => $adrLivr->getAdresse1(),
-            'address2' => $adrLivr->getAdresse2(),
-            'postcode' => $adrLivr->getCp(),
-            'city' => $adrLivr->getVille(),
-            'country' => $adrLivr->getPays(),
-            'delivery_type' => $delivery_type,
-        );
-        
+
+        $details['metadata'] = $payment->getDetails()['metadata'];
+        $details['billing'] = $payment->getDetails()['billing'];
+        $details['shipping'] = $payment->getDetails()['shipping'];
+
         $request->setResult((array) $details);
     }
 
